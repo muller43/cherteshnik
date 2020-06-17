3b9fb4fa7407bbfc08bc10da25bb96c46f5a6353
muller43 <bbc8304@gmail.com>
2020-06-17 19:15:22 +0300

    сделан чертежник из черепахи

---
+++ drawman.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+t=turtle()
+
+def test_draman():
+    """тестирование работы Чертежника
+    :return:None
+    """
+    pen_down()
+
+
+
+
+
+
+
+if name=='___mane___':
+    test_drawman()
+
